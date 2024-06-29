dd42c67d090530bafe32590778f9c36850516a5a
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-06-29 10:50:51 -0400

    Added css

--- Pages/Categories/Create.cshtml.cs
+++ Pages/Categories/Create.cshtml.cs
@@ -2,40 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AbbyWeb.Data;
-using AbbyWeb.Model;
+using Abbyweb.Data; // Corrected namespace
+using Abbyweb.Model; // Corrected namespace
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace AbbyWeb.Pages.Categories;
-
-[BindProperties]
-public class CreateModel : PageModel
+namespace AbbyWeb.Pages.Categories
 {
-    private readonly ApplicationDbContext _db;
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
     
-    public Category Category { get; set; }
+        public Category Category { get; set; }
 
-    public CreateModel(ApplicationDbContext db)
-    {
-        _db = db;
-    }
-    public void OnGet()
-    {
-    }
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
-    public async Task<IActionResult> OnPost()
-    {
-        if (Category.Name == Category.DisplayOrder.ToString())
+        public void OnGet()
         {
-            ModelState.AddModelError("Category.Name", "The DisplayOrder cannot exactly match the Name.");
         }
-        if (ModelState.IsValid)
+
+        public async Task<IActionResult> OnPost()
         {
-            await _db.Category.AddAsync(Category);
-            await _db.SaveChangesAsync();
-            return RedirectToPage("Index");
+            if (Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Category.Name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                await _db.Category.AddAsync(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
-        return Page();
     }
 }
