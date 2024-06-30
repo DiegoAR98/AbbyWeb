c9180e9064fd05b84ea4e65bb3ac3a1ea814f003
DiegoAR98 <146890514+DiegoAR98@users.noreply.github.com>
2024-06-30 11:29:12 -0400

    deploying

--- Program.cs
+++ Program.cs
@@ -1,15 +1,66 @@
 using Abbyweb.Data;
-using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
-
+using Npgsql;
+using System;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Load .env file in development
+if (builder.Environment.IsDevelopment())
+{
+    DotNetEnv.Env.Load();
+}
+
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DefaultConnection")
-));
+
+// Configure the database connection
+string connectionString;
+
+if (builder.Environment.IsDevelopment())
+{
+    // Use local connection string from .env
+    var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    var databaseUri = new Uri(databaseUrl);
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    var npgsqlBuilder = new NpgsqlConnectionStringBuilder
+    {
+        Host = databaseUri.Host,
+        Port = databaseUri.Port,
+        Username = userInfo[0],
+        Password = userInfo[1],
+        Database = databaseUri.LocalPath.TrimStart('/'),
+        SslMode = SslMode.Require,
+        TrustServerCertificate = true
+    };
+
+    connectionString = npgsqlBuilder.ToString();
+}
+else
+{
+    // Use Heroku provided connection string
+    var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+    var databaseUri = new Uri(databaseUrl);
+    var userInfo = databaseUri.UserInfo.Split(':');
+
+    var npgsqlBuilder = new NpgsqlConnectionStringBuilder
+    {
+        Host = databaseUri.Host,
+        Port = databaseUri.Port,
+        Username = userInfo[0],
+        Password = userInfo[1],
+        Database = databaseUri.LocalPath.TrimStart('/'),
+        SslMode = SslMode.Require,
+        TrustServerCertificate = true
+    };
+
+    connectionString = npgsqlBuilder.ToString();
+}
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
@@ -17,7 +68,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
